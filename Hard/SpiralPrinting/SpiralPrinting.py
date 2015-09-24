7e6a7d6f26b9a2a33dabc003cd58d6f0b6ce7df3
marinofranusic <marinofranusic@gmail.com>
2015-09-24 10:21:39 +0200

    SpiralPrinting challenge solution
    Running through the matrix in several passes (left->right, up->down,
    right->left, down->up) and counting the number of printed elements. When
    I print all of them, stop.

--- Hard/SpiralPrinting/SpiralPrinting.py
+++ Hard/SpiralPrinting/SpiralPrinting.py
@@ -33,12 +33,6 @@ def GiveSubMatrix(matrix,startRow,startColumn,endRow,endColumn):
             counter+=1
     return ret
 
-def PrintNormalMatrix(matrix):
-    for i in range(len(matrix)):
-        for j in range (len(matrix[i])):
-            print(str(matrix[i][j])+" " ,end='')
-        print()
-
 def CreateMatrix(M,N,items):
     arr=items.split(' ')
     row=0
@@ -66,8 +60,6 @@ def PrintItemsReverse(itemList):
         counter+=1
     return counter
 
-def HelperValuesOut(rb,re,cb,ce,c):
-    print("Row begin: %d, Row end: %d, Column begin: %d, Column end: %d, Counter: %d" % (rb,re,cb,ce,c))
 
 def main(argv=None):
   with open(sys.argv[1]) as f:
@@ -81,7 +73,6 @@ def main(argv=None):
               print(items[2])
           else:
               mat=CreateMatrix(M,N,items[2])
-              #PrintNormalMatrix(mat)
               row_begin=0
               col_begin=0
               row_end=M-1
@@ -91,22 +82,19 @@ def main(argv=None):
                   #Left->right
                   counter+=PrintItems(GiveSubMatrix(mat,row_begin,col_begin,row_begin,col_end))
                   row_begin+=1
-                  #HelperValuesOut(row_begin,row_end,col_begin,col_end,counter)
                   if counter>=M*N: break
                   #Up->down
                   counter+=PrintItems(GiveSubMatrix(mat,row_begin,col_end,row_end,col_end))
                   col_end-=1
-                  #HelperValuesOut(row_begin,row_end,col_begin,col_end,counter)
                   if counter>=M*N: break
                   #Right->left
                   counter+=PrintItemsReverse(GiveSubMatrix(mat,row_end,col_begin,row_end,col_end))
                   row_end-=1
-                  #HelperValuesOut(row_begin,row_end,col_begin,col_end,counter)
                   if counter>=M*N: break
                   #Down->up
                   counter+=PrintItemsReverse(GiveSubMatrix(mat,row_begin,col_begin,row_end,col_begin))
                   col_begin+=1
-                  #HelperValuesOut(row_begin,row_end,col_begin,col_end,counter)
+                  
           print()
       
           
